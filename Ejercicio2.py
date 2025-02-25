d8d738d523e60d18d97cf100ea8cc856462d223a
Alejandro <up240082@alumnos.upa.edu.mx>
2025-02-25 09:32:55 -0600

    Avance dia 4

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,13 +1,34 @@
 #Dia 4
 #Programa 1
-print (programa 1)
-palabra1 = "Thirty "
+print ("programa 1")
+palabra1 = "'Thirty "
 palabra2 = "Days "
 palabra3 = "Of "
-palabra4 = "Python"
+palabra4 = "Python'"
 resultado = palabra1 + palabra2 + palabra3 + palabra4
 print ("La frase es: ", resultado)
 
 #Programa 2
-print (programa 2)
-palabra1 = ""
+print ("programa 2")
+palabra1 = "'Coding "
+palabra2 = "For "
+palabra3 = "All'"
+resultado = palabra1 + palabra2 + palabra3
+print ("el resultado es: ", resultado)
+
+#Programa 3
+print ("programa 3")
+company = "Coding for all"
+
+#Programa 4
+print ("programa 4")
+print (company)
+
+#Programa 5
+print ("programa 5")
+company = "Coding for all"
+length = len(company)
+print ("La longitud de 'coding for all es: ", length)
+
+#Programa 6
+print ("programa 6")
