4720fd1ca572bfdecb0ced710562247c95ecb9b4
Alejandro <up240082@alumnos.upa.edu.mx>
2025-02-25 09:21:33 -0600

    .

---
+++ Ejercicio2.py
@@ -0,0 +1,13 @@
+#Dia 4
+#Programa 1
+print (programa 1)
+palabra1 = "Thirty "
+palabra2 = "Days "
+palabra3 = "Of "
+palabra4 = "Python"
+resultado = palabra1 + palabra2 + palabra3 + palabra4
+print ("La frase es: ", resultado)
+
+#Programa 2
+print (programa 2)
+palabra1 = ""
