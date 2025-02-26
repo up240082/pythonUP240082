5909ee14abb5a26979c6ed35f3d4e1e51721feb8
Alejandro <up240082@alumnos.upa.edu.mx>
2025-02-26 17:32:34 -0600

    avance

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,6 +1,6 @@
 #Dia 4
 #Programa 1
-print ("programa 1")
+print ("programa 1: ")
 palabra1 = "'Thirty "
 palabra2 = "Days "
 palabra3 = "Of "
@@ -9,7 +9,7 @@ resultado = palabra1 + palabra2 + palabra3 + palabra4
 print ("La frase es: ", resultado)
 
 #Programa 2
-print ("programa 2")
+print ("programa 2: ")
 palabra1 = "'Coding "
 palabra2 = "For "
 palabra3 = "All'"
@@ -17,18 +17,42 @@ resultado = palabra1 + palabra2 + palabra3
 print ("el resultado es: ", resultado)
 
 #Programa 3
-print ("programa 3")
+print ("programa 3: ")
 company = "Coding for all"
 
 #Programa 4
-print ("programa 4")
+print ("programa 4: ")
 print (company)
 
 #Programa 5
-print ("programa 5")
+print ("programa 5: ")
 company = "Coding for all"
 length = len(company)
 print ("La longitud de 'coding for all es: ", length)
 
 #Programa 6
-print ("programa 6")
+print ("programa 6: ")
+min = "'metodologia de la programacion'"
+mayus = min.upper()
+print ("el texto en minusculas es: ", min)
+print ("el texto en mayusculas es: ", mayus)
+
+#Programa 7
+print ("programa 7: ")
+mayus = "'METODOLOGIA DE LA PROGRAMACION'"
+minus = mayus.lower()
+print ("EL TEXTO EN MAYUSCULAS ES: ", mayus)
+print ("el texto en minusculas es : ", minus)
+
+#programa 8
+print ("programa 8: ")
+string = "'Coding For All'"
+stringCapitalize = string.capitalize()
+stringTitle = string.title()
+stringSwapcase = string.swapcase()
+print ("funcion capitalize: ", stringCapitalize)
+print ("funcion title: ", stringTitle)
+print ("funcion stringSwapcase: ", stringSwapcase)
+
+#Programa 9
+print ("programa 9:")
