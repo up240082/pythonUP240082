89f157420a336d12993cf05992ae557b94e8b421
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-04 09:00:57 -0600

    inicio

---
+++ Ejercicio3.py
@@ -0,0 +1,18 @@
+#Ejerciciso dia 4
+
+#Programa 1
+print ("programa 1:")
+listaVacia = []
+print (listaVacia)
+
+#Programa 2
+print ("programa 2:")
+lista = (1, 2, 3, 4, 5)
+print (lista)
+
+#Programa 3
+print ("programa 3:")
+print (len(lista))
+
+#Programa 4
+print ("programa 4:")
