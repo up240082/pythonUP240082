b58aa5cf043a69cd73e35971b761e94da1d38427
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-05 10:33:17 -0600

    avance

--- Dia 5.py
+++ Dia 5.py
@@ -7,7 +7,7 @@ print (listaVacia)
 
 #Programa 2
 print ("programa 2:")
-lista = (1, 2, 3, 4, 5)
+lista = ('banana', 'apple', 'orange', 'lemon', 'mango')
 print (lista)
 
 #Programa 3
@@ -16,3 +16,98 @@ print (len(lista))
 
 #Programa 4
 print ("programa 4:")
+print (lista[0])
+print (lista[2])
+print (lista [4])
+
+
+#Programa 5
+print ("programa 5:")
+mixed_data_types = 'Alejandro', '18', '1.77m', 'Casado', 'Paseo San Gerardo #150'
+
+
+
+#Programa 6
+print ("programa 6:")
+it_companies =  ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+
+#Programa 7
+print ("programa 7:")
+print (it_companies)
+print (mixed_data_types)
+
+#Programa 8
+print ("programa 8:")
+print (len(it_companies))
+
+#Programa 9
+print ("programa 9:")
+print (it_companies[0])
+print (it_companies[3])
+print (it_companies[6])
+
+#Programa 10
+print ("programa 10:")
+it_companiesDos =   ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+it_companiesDos [0] = 'Twitter'
+print (it_companiesDos)
+
+#Programa 11
+print ("programa 11:")
+it_companiesTres =  ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+it_companiesTres.append('Snapchat')
+print (it_companiesTres)
+
+#Programa 12
+print ("programa 12:")
+it_companiesTres.insert (4, 'Instagram')
+print (it_companiesTres)
+
+#Programa 13
+print ("programa 12:")
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+mayus = it_companies[3].upper()
+print (mayus)
+
+#Programa 14
+print ("programa 14:")
+print ('#'.join(it_companies))
+
+
+#Programa 15
+print ("programa 15:")
+comparar = 'Apple' in it_companies
+print (comparar)
+
+#Programa 16
+print ("programa 16:")
+it_companies.sort()
+print (it_companies)
+
+
+#Programa 17
+print ("programa 17:")
+it_companies.reverse()
+print (it_companies)
+
+#Programa 18
+print ("programa 18:")
+cutCompanies = it_companies[3:]
+print (cutCompanies)
+
+#Programa 19
+print ("programa 19:")
+cutCompanies = it_companies[0:4]
+print (cutCompanies)
+
+#Programa 20
+print ("programa 20:")
+
+
+
+
+
+
+
+
