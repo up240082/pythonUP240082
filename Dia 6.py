a88b68c0d26f025b356dcbc761ca5de2066e6740
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-11 09:12:32 -0600

    final

--- Dia 6.py
+++ Dia 6.py
@@ -29,5 +29,44 @@ familyMembers= padres + hermanos
 print (familyMembers)
 
 #Ejercicios nivel 2
+#Ejercicio 1
 print ('programa 1:')
-unPack familyMembers [1]
+unPack = familyMembers [0:2]
+print (unPack)
+unPackDos = familyMembers [2:6]
+print (unPackDos)
+
+#Ejercicio 2
+print ('programa 2:')
+fruits = ('manzana', 'pera', 'naranja')
+vegetables = ('lechuga', 'jitomate', 'zanahoria')
+animals = ('jp', 'cebra', 'leon')
+food_stuff_tp = fruits + vegetables + animals
+print (food_stuff_tp)
+
+#Ejercicio 3
+print ('programa 3:')
+food_stuff_it = ('manzana', 'pera', 'naranja', 'lechuga', 'jitomate', 'zanahoria', 'jp', 'cebra', 'leon')
+print (food_stuff_it)
+
+#Ejercicio 4
+print ('programa 4:')
+print ('nueva lista: ', food_stuff_it[0:3]+ food_stuff_it[5:9])
+
+#Ejercicio 5
+print ("programa 5:")
+print ('nueva lista: ', food_stuff_it[3:6])
+
+#Ejercicio 6
+print ('programa 6:')
+del food_stuff_tp
+
+#Ejercicio 7
+print ('programa 7:')
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+comprueba = ('estonia' in nordic_countries)
+compruebaDos = ('Iceland' in nordic_countries)
+print ("Estonia se encuentra en la lista?: ", comprueba)
+print ("iceland se encuentra en la lista?: ", compruebaDos)
+
+
