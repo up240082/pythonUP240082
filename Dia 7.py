5d17d1f4833f851a9a6f0bf0e64ff497a582f723
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-11 09:28:41 -0600

    avance

---
+++ Dia 7.py
@@ -0,0 +1,24 @@
+#Ejercicios dia 7
+
+#Ejercicio 1
+print ('programa 1:')
+it_companies = ('Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazo')
+
+#Ejercicio 2
+print ('programa 2:')
+add = ('twitter',)
+it_companiesDos = it_companies + add
+print (it_companiesDos)
+
+#Ejercicio 3
+print ("programa 3:")
+empresasDigitales = ('snapchat', 'tiktok', 'instagram')
+it_companiesTres = (it_companiesDos + empresasDigitales)
+print (it_companiesTres)
+
+#Ejericicio 4
+print ('programa 4:')
+print (it_companiesTres[1:11])
+
+#Ejercicio 5
+print ('programa 5:')
