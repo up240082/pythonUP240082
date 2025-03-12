6480bb5ccd40caa2815b9b97672b1ddfbba31f28
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-12 09:54:58 -0600

    Avance

--- Dia 6.py
+++ Dia 6.py
@@ -29,14 +29,14 @@ familyMembers= padres + hermanos
 print (familyMembers)
 
 #Ejercicios nivel 2
-#Ejercicio 1
+#Programa 1
 print ('programa 1:')
 unPack = familyMembers [0:2]
 print (unPack)
 unPackDos = familyMembers [2:6]
 print (unPackDos)
 
-#Ejercicio 2
+#Programa 2
 print ('programa 2:')
 fruits = ('manzana', 'pera', 'naranja')
 vegetables = ('lechuga', 'jitomate', 'zanahoria')
@@ -44,24 +44,24 @@ animals = ('jp', 'cebra', 'leon')
 food_stuff_tp = fruits + vegetables + animals
 print (food_stuff_tp)
 
-#Ejercicio 3
+#Programa 3
 print ('programa 3:')
 food_stuff_it = ('manzana', 'pera', 'naranja', 'lechuga', 'jitomate', 'zanahoria', 'jp', 'cebra', 'leon')
 print (food_stuff_it)
 
-#Ejercicio 4
+#Programa 4
 print ('programa 4:')
 print ('nueva lista: ', food_stuff_it[0:3]+ food_stuff_it[5:9])
 
-#Ejercicio 5
+#Programa 5
 print ("programa 5:")
 print ('nueva lista: ', food_stuff_it[3:6])
 
-#Ejercicio 6
+#Programa 6
 print ('programa 6:')
 del food_stuff_tp
 
-#Ejercicio 7
+#Programa 7
 print ('programa 7:')
 nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 comprueba = ('estonia' in nordic_countries)
--- Dia 7.py
+++ Dia 7.py
@@ -1,24 +1,82 @@
 #Ejercicios dia 7
 
-#Ejercicio 1
+#Programa 1
 print ('programa 1:')
 it_companies = ('Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazo')
 
-#Ejercicio 2
+#Programa 2
 print ('programa 2:')
 add = ('twitter',)
 it_companiesDos = it_companies + add
 print (it_companiesDos)
 
-#Ejercicio 3
+#Programa 3
 print ("programa 3:")
 empresasDigitales = ('snapchat', 'tiktok', 'instagram')
 it_companiesTres = (it_companiesDos + empresasDigitales)
 print (it_companiesTres)
 
-#Ejericicio 4
+#Programa 4
 print ('programa 4:')
 print (it_companiesTres[1:11])
 
-#Ejercicio 5
+#Programa 5
 print ('programa 5:')
+#Retirar (remove) implica sacar un elemento de una lista
+#Desechar elimina un elemento solo si esta presente, si no lo esta, no lanza error
+
+#Ejercicios nivel 2
+#Programa 1
+print ("programa 1, nivel 2:")
+A = ['19', '22', '24', '20', '25', '26']
+B = ['19', '22', '20', '25', '26', '24', '28', '27']
+AB = A + B
+print (AB)
+
+#Programa 2
+print ('programa 2, nivel 2:')
+A = {'19', '22', '24', '20', '25', '26'}
+B = {'19', '22', '20', '25', '26', '24', '28', '27'}
+print (A.intersection(B))
+
+#Programa 3
+print ("programa 3, nivel 2:")
+A = {'19', '22', '24', '20', '25', '26'}
+B = {'19', '22', '20', '25', '26', '24', '28', '27'}
+print ("Es A subconjunto de B?: " ,A.issubset(B))
+print (A.issuperset(B))
+
+#Programa 4
+print ("programa 4, nivel 2:")
+A = {'19', '22', '24', '20', '25', '26'}
+B = {'19', '22', '20', '25', '26', '24', '28', '27'}
+print (A.isdisjoint(B))
+
+#Programa 5
+print ("programa 5, nivel 2:")
+A = ['19', '22', '24', '20', '25', '26']
+B = ['19', '22', '20', '25', '26', '24', '28', '27']
+BA = B + A
+print ("A + B: ",AB)
+print ("B + A",BA)
+
+#Programa 6
+print ("programa 6, nivel 2:")
+A = {'19', '22', '24', '20', '25', '26'}
+B = {'19', '22', '20', '25', '26', '24', '28', '27'}
+print (A.symmetric_difference(B))
+
+
+#Programa 7
+print ("programa 7, nivel 2:")
+AB.clear()
+BA.clear()
+print (AB + BA)
+
+#Ejercicios nivel 3
+#Programa 1
+print ("programa 1, nivel 3:")
+ages = ['22', '19', '24', '25', '26', '24', '25', '24']
+
+
+
