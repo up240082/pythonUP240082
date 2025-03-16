ac8a3a3741cfa46c160fb0087ab9a82df74ae794
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-16 12:23:57 -0600

    avance

--- 08_Day/Dia 8.py
+++ 08_Day/Dia 8.py
@@ -1,1 +1,33 @@
 #Ejercicios dia 8
+#Programa 1
+print ("programa 1:")
+perro = {}
+
+#Programa 2
+print ("programa 2:")
+perro ['nombre'] = 'toby'
+perro ['color'] = 'cafe'
+perro ['raza'] = 'chihuahua'
+perro ['patas'] = '4'
+perro ['edad'] = '5 años'
+print (perro)
+
+#Programa 3
+print ("programa 3:")
+ 
+estudiante = {}
+estudiante ['nombre'] = 'Alejandro'
+estudiante ['apellido'] = 'Lopez'
+estudiante ['sexo'] = 'masculino'
+estudiante ['edad'] = '18 años'
+estudiante ['estado civil'] = 'soltero'
+estudiante ['habilidades'] = 'veloz e inteligente'
+estudiante ['pais'] = 'Italia'
+estudiante ['ciudad'] = 'Aguascalientes'
+estudiante ['direccion'] = 'Paseo San Gerardo #150'
+print (estudiante)
+
+#Programa 4
+print ("programa 4:")
+print (len(estudiante))
+    
