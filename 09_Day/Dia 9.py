aa52a50d9a052b2e3415940ba6be2e0d3af73526
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-19 10:37:48 -0600

    Dia 9

--- 09_Day/Dia 9.py
+++ 09_Day/Dia 9.py
@@ -1,8 +1,8 @@
 #Ejercicios dia 9
 
 #Programa 1
-print ('programa 9:')
-edad = float(input("Ingresa tu edad: "))
+print ('programa 1:')
+edad = int(input("Ingresa tu edad: "))
 if edad > 17:
     print ("Puedes manejar")
 else:
@@ -10,12 +10,62 @@ else:
 
 #Programa 2
 print ('programa 2:')
-tuEdad= float(input("Ingresa tu edad: "))
-if edad > tuEdad:
-    print ("Mi edad es mayor por:", tuEdad - edad ,"años")
+miEdad = int(input("Ingresa mi edad: "))
+tuEdad= int(input("Ingresa tu edad: "))
+if miEdad > tuEdad:
+    print ("Mi edad es mayor: ", miEdad-tuEdad)
+elif miEdad == tuEdad:
+    print ("Son la misma edad: ")
 else:
-    print ("Tu edad es mayor por: ", tuEdad - edad, "años")
+    print ("Tu edad es mayor: ", miEdad-tuEdad)
 
-#
+#Programa 3
+print ("programa 3:")
+a = int(input("Ingresa el primer numero: " ))
+b = int(input("Ingresa el segundo numero: "))
+if a > b:
+    print ("El numero 'a' es mayor: ")
+elif a == b:
+    ("Los numeros son iguales: ")
+else:
+  print  ("El numero 'b' es mayor: ")
 
+#Ejercicios nivel 2
+#Programa 1
+print ("programa 1:")
+alumno = int(input("Ingrese la calificacion del alumno: "))
+if alumno > 79 and alumno < 100:
+    print ("El alumno tiene un 'A'")
+elif alumno > 69 and alumno < 80:
+    print ("El alumno tiene una 'B'")
+elif alumno > 59 and alumno < 70:
+    print ("El alumno tiene una 'C'")
+elif alumno > 49 and alumno < 60:
+    print ("El alumno tiene una 'D'")
+else:
+    print ("El alumno tiene una 'F'")
 
+#Programa 2
+print ("programa 2:")
+mes = (input("Ingresa el  mes: "))
+if mes in ['septiembre', 'octubre', 'noviembre',]:
+    print ("La estacion es otoño")
+elif mes in ['diciembre', 'enero', 'febrero',]:
+    print ("La estacion es invierno")
+elif mes in ['marzo','abril', 'mayo',]:
+    print ("La estacion es primavera")
+else:
+    print ("La estacion es verano")
+
+#Programa 3
+print ("programa 3:")
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruta = str(input("Ingresa la fruta: "))
+if fruta in fruits:
+    print ("La fruta esta en la lista")
+else:
+    fruits.append(fruta)
+    print (fruits)
+
+#Ejercicios nivel 3
+#Programa 1
---
+++ 10_Day/Dia 10.py
@@ -0,0 +1,11 @@
+mes = (input("Ingresa el  mes: "))
+if mes in ['septiembre', 'octubre', 'noviembre']:
+    print ("La estacion es otoño")
+elif mes in ['diciembre', 'enero', 'febrero']:
+    print ("La estacion es invierno")
+elif mes in ['marzo', 'abril', 'mayo']:
+    print ("La estacion es primavera")
+elif mes['junio', 'julio', 'agosto']:
+    print ("La estacion es verano")
+else:
+    print ("No hay estacion")
