ab8cbad8c0210f82a36f6385b1d049902e915320
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-26 10:36:52 -0600

    avance

--- 10_Day/Dia 10.py
+++ 10_Day/Dia 10.py
@@ -80,38 +80,12 @@ print ("La suma de los impares es: ", sumaImpares)
 #Ejercicios nivel 2
 #Programa 1
 print ("programa 1, nivel 3:")
-countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda',
-    'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain',
-    'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia',
-    'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso',
-    'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic',
-    'Chad', 'Chile', 'China', 'Colombi', 'Comoros', 'Congo (Brazzaville)', 'Congo',
-    'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark',
-    'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor (Timor Timur)', 'Ecuador',
-    'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji',
-    'Finland', 'France', 'Gabon', 'Gambia, The', 'Georgia', 'Germany', 'Ghana', 'Greece',
-    'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras',
-    'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy',
-    'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North',
-    'Korea, South', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia',
-    'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi',
-    'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius',
-    'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Morocco', 'Mozambique',
-    'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua',
-    'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea',
-    'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia',
-    'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent', 'Samoa', 'San Marino',
-    'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia and Montenegro', 'Seychelles',
-    'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia',
-    'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden',
-    'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga',
-    'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine',
-    'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan',
-    'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe',]
+import paises as p
+paises = p.countries
 land = 'land'
-for countrie in countries:
-    if land in countrie:
-        print (countrie)
+for pais in paises:
+    if land in pais:
+        print (pais)
 
 #Programa 2
 print ("programa 2, nivel 3:")
@@ -124,8 +98,14 @@ print (reverseFruits)
 
 #Programa 3
 print ("programa 3, nivel 3:")
-import countries_data as data
-datos = data.paises
+import countriesData as countrie
+paisData = countrie.paisD
+lenguajes = []
+for pais in lenguajes:
+    for lenguaje in pais:
+        lenguajes.append("languages")
+
+print (lenguajes)
 
 
 
---
+++ 11_Day/Dia 11.py
@@ -0,0 +1,22 @@
+#Ejercicios dia 11
+#Programa 1
+print ("programa 1:")
+def add_two_numbers():
+    num_one = float(input("ingresa un numero: "))
+    num_two = float(input("ingresa un numero: "))
+    total = num_one + num_two
+    print('resultado: ', total)
+add_two_numbers()
+
+#Programa 2
+print ("programa 2:")
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    return area
+rad=  float(input("ingrese el radio: "))
+print(area_of_circle(rad))
+
+#Programa 3
+print ("programa 3:")
+
