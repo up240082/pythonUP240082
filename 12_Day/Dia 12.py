773315b158fdfe975e891afc377bc3b3049c1712
Alejandro <up240082@alumnos.upa.edu.mx>
2025-03-30 21:15:15 -0600

    ya merito

--- 12_Day/Dia 12.py
+++ 12_Day/Dia 12.py
@@ -1,13 +1,3 @@
-def calcular_modo(numeros):
-    from collections import Counter
-    frecuencias = Counter(numeros)
-    max_frecuencia = max(frecuencias.values())
-    modos = [num for num, freq in frecuencias.items() if freq == max_frecuencia]
-    return modos
-
-# Lista de números
-numeros = [4, 5, 6, 4, 7, 4]
-
-# Calcular y mostrar el modo
-modos = calcular_modo(numeros)
-print(f"El modo es: {modos}")
+#Ejericicios dia 12
+#Programa 1
+print ("programa 1:")
