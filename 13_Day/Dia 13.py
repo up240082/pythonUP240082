2ad00f356dd07c1631df051a98f67a3e844a3938
Alejandro <up240082@alumnos.upa.edu.mx>
2025-04-02 09:42:07 -0600

    finaaaal

--- 13_Day/Dia 13.py
+++ 13_Day/Dia 13.py
@@ -14,4 +14,32 @@ print (lista)
 #Programa 3
 print ("programa 3:")
 tuplesList = [(i, 1, i**1, i**2, i**3, i**4, i**5) for i in range(11)]
-print  (tuplesList)
+print(tuplesList)
+
+#Programa 4
+print ("programa 4:")
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+lista = [i for i in countries for j in i for i in j]
+listaDos = [i.upper() for i in lista]
+print (listaDos)
+
+#Programa 5
+print ("programa 5:")
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+diccCountry = [{'country': country.upper(), 'city': city.upper() }for[(country, city)] in countries]
+print (diccCountry)
+
+#Programa 6
+print ("programa 6:")
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+listaa = [''.join(i)for j in names for i in j]
+print (listaa)
+
+#Programa 7
+print ("programa 7:")
+slope = lambda x1, y1, x2, y2: (y2 - y1) / (x2 - x1) if x2 != x1 else None
+y_intercept = lambda m, x1, y1: y1 - m * x1
+m = slope(1, 2, 3, 6)
+b = y_intercept(m, 1, 2)
+print("Slope:", m)
+print("Y-Intercept:", b)
