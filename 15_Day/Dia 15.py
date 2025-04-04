f54ec3a3d9c2ecef61ebe1d7241bd3aaada7de51
Alejandro <up240082@alumnos.upa.edu.mx>
2025-04-03 18:47:15 -0600

    yaaaaaa

--- 15_Day/Dia 15.py
+++ 15_Day/Dia 15.py
@@ -1,6 +1,19 @@
-def isPythonVariable(variable):
-    if variable.isidentifier():
-        return True
-    else:
-        return False
-print(isPythonVariable(variable='variable1'))
+#Ejercicios dia 15
+#Programa 1
+print ("programa 1:")
+print ("hola" #Falta el parentesis de cierre
+       
+print (nombre) #"nombre" no esta definido
+
+print ("hola" + 10) #No se puede concatenar una cadena con un numero
+
+diccionario = {"nombre": "Alejandro"}
+print (diccionario ["edad"]) #edad no esta en el diccionario
+
+int("hola") #No se puede convertir "hola" a entero
+
+import modulo_inexistente #no existe
+ 
+ print (se acabo el cuatri) #No esta definido
+
+ print ("arriba las chivas")
