1537360574c557c6496fb7f66146b5fc6a76baee
Alejandro <up240082@alumnos.upa.edu.mx>
2025-04-03 23:10:42 -0600

    porfin

--- 15_Day/Dia 15.py
+++ 15_Day/Dia 15.py
@@ -1,19 +1,38 @@
 #Ejercicios dia 15
 #Programa 1
 print ("programa 1:")
+#1
 print ("hola" #Falta el parentesis de cierre
        
+#2       
 print (nombre) #"nombre" no esta definido
 
+#3
 print ("hola" + 10) #No se puede concatenar una cadena con un numero
 
+#4
 diccionario = {"nombre": "Alejandro"}
 print (diccionario ["edad"]) #edad no esta en el diccionario
 
+#5
 int("hola") #No se puede convertir "hola" a entero
 
+#6
 import modulo_inexistente #no existe
  
+ #7
  print (se acabo el cuatri) #No esta definido
 
- print ("arriba las chivas")
+#8
+ print (arriba las chivas)
+
+#9
+resultado = "10" + 5 #se intenta sumar un numero con un string
+
+#10
+lista = [1, 2, 3]
+print(lista[5])
+
+#11
+numero = 10
+print(numero.upper())  # `int` no tiene el método `upper()`
